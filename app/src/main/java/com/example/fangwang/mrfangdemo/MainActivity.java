8cf5abb00505d523dd9240d63d224b6dd49887de
房子君 <610492614@qq.com>
2018-05-07 00:06:56 +0800

    主分支文件修改

--- app/src/main/java/com/example/fangwang/mrfangdemo/MainActivity.java
+++ app/src/main/java/com/example/fangwang/mrfangdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "我靠", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "这是主分支的代码", Toast.LENGTH_SHORT).show();
     }
 }
